e319cf3e532565a3fad85e14845fc82548a4fc95
MyManVan <kcjackkelly@gmail.com>
2020-04-30 15:25:24 -0500

    Removed unnessecary git attributes

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Jack's Peppers</h1>
+      <h3>Pepper Seed Supplier</h3>
+      <Link to="/shop">Shop</Link>
     </div>
   );
 };
