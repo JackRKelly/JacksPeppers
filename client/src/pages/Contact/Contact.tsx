ca2445cae590198ac8e7c3933283cad905975689
MyManVan <kcjackkelly@gmail.com>
2020-04-30 20:22:44 -0500

    Added main section styling for all pages

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.scss";
 
 const Contact: React.FC = () => {
   document.title = "Contact | Jack's Peppers";
--- client/src/pages/Shop/Shop.tsx
+++ client/src/pages/Shop/Shop.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.scss";
 
 const Shop: React.FC = () => {
   document.title = "Shop | Jack's Peppers";
