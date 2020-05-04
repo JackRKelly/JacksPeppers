2653e15a81dfb327ea499856c31bf830aabac6aa
MyManVan <kcjackkelly@gmail.com>
2020-05-03 19:12:56 -0500

    Added product page base template

--- client/src/pages/Product/Product.tsx
+++ client/src/pages/Product/Product.tsx
@@ -8,9 +8,18 @@ const Product: React.FC = () => {
   document.title = `Pepper Name | Jack's Peppers`;
   console.log(id);
 
+  const pepper = {
+    title: "Sugar Rush Red",
+    price: 2.5,
+    inStock: true,
+    color: "#FF7937",
+    image: "sugar-red.jpg",
+  };
+
   return (
     <main className="product">
-      <h1>Product</h1>
+      <h1>{pepper.title}</h1>
+      <h3>{pepper.price}</h3>
     </main>
   );
 };
