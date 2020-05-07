0f88587b437942d91dc7c22eece705ece9977187
MyManVan <kcjackkelly@gmail.com>
2020-05-07 13:24:30 -0500

    Added RC Slider for advanced search

--- client/src/pages/Shop/Shop.tsx
+++ client/src/pages/Shop/Shop.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./index.scss";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const Shop: React.FC = () => {
   document.title = "Shop | Jack's Peppers";
+
+  const [showAdvanced, setShowAdvanced] = useState(false);
+
+  const [priceRange, setPriceRange] = useState([0, 5]);
+  const [heatRange, setHeatRange] = useState([0, 5]);
 
   return (
     <main className="shop">
@@ -21,6 +28,9 @@ const Shop: React.FC = () => {
               <button
                 className="search-settings--form-basic--advanced"
                 type="button"
+                onClick={() => {
+                  setShowAdvanced(!showAdvanced);
+                }}
               >
                 Advanced
               </button>
@@ -29,7 +39,45 @@ const Shop: React.FC = () => {
                 type="submit"
               ></button>
             </div>
-            <div className="search-settings-form-advanced"></div>
+            <div
+              className="search-settings--form-advanced"
+              style={{
+                maxHeight: showAdvanced ? `250px` : "0",
+                padding: showAdvanced ? "10px" : "0",
+              }}
+            >
+              <div className="search-settings--form-advanced--price">
+                <p>
+                  Price Range: ${priceRange[0].toFixed(2)} - $
+                  {priceRange[1].toFixed(2)}
+                </p>
+                <Range
+                  min={0}
+                  max={20}
+                  onChange={(value) => {
+                    setPriceRange(value);
+                  }}
+                  step={0.5}
+                  defaultValue={[0, 5]}
+                  allowCross={false}
+                />
+              </div>
+              <div className="search-settings--form-advanced--heat">
+                <p>
+                  Heat Range: {heatRange[0]} - {heatRange[1]}
+                </p>
+                <Range
+                  min={0}
+                  max={5}
+                  onChange={(value) => {
+                    setHeatRange(value);
+                  }}
+                  step={1}
+                  defaultValue={[0, 5]}
+                  allowCross={false}
+                />
+              </div>
+            </div>
           </form>
         </div>
       </header>
