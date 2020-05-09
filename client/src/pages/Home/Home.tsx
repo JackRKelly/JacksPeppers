6d211e06c663a392aceb493eef667d428eabdcac
MyManVan <kcjackkelly@gmail.com>
2020-05-09 16:33:18 -0500

    Converted shop list to css grid

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -16,57 +16,6 @@ const Home: React.FC = () => {
           Shop
         </Link>
       </header>
-
-      <div className="featured-products">
-        <h3>Featured Products:</h3>
-        <div className="featured-list">
-          <ProductCard
-            id={53}
-            title="Sugar Rush Red"
-            heat={1}
-            price={2.5}
-            inStock={true}
-            colorList={[ColorKind.Red, ColorKind.Orange]}
-            image="sugar-red.jpg"
-          />
-          <ProductCard
-            id={55}
-            title="Pink Tiger"
-            heat={2}
-            price={4}
-            inStock={false}
-            colorList={[ColorKind.Pink, ColorKind.Peach]}
-            image="pinktiger.jpg"
-          />
-          <ProductCard
-            id={24}
-            title="7 Pot White"
-            heat={3}
-            price={3.5}
-            inStock={true}
-            colorList={[ColorKind.White]}
-            image="7potwhite.jpg"
-          />
-          <ProductCard
-            id={24}
-            title="Roxa Lantern Black"
-            heat={4}
-            price={3.5}
-            inStock={true}
-            colorList={[ColorKind.Purple, ColorKind.Black]}
-            image="roxa-black.jpg"
-          />
-          <ProductCard
-            id={24}
-            title="Roxa Lantern Black"
-            heat={5}
-            price={3.5}
-            inStock={true}
-            colorList={[ColorKind.Purple, ColorKind.Black]}
-            image="roxa-black.jpg"
-          />
-        </div>
-      </div>
     </main>
   );
 };
