e32d8f096c65a452c24dda28dabc900a316eed86
MyManVan <kcjackkelly@gmail.com>
2020-05-10 17:00:02 -0500

    Added fs, and path. Added build html to index route.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
-import express, { Application, Router } from "express";
+import express, { Application } from "express";
 
 const app: Application = express();
 
 import index from "./routes/index";
+import inventory from "./routes/inventory";
 
 app.use("/", index);
+app.use("/inventory", inventory);
 
 app.listen(5000, () => {
   console.log("Server live at port 5000");
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express";
-
+import fs from "fs";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  res.type("html");
+  fs.readFile("../../client/build/index.html", (err, data: Buffer) => {
+    console.log(err);
+    res.send(data);
+  });
 });
 
 export default router;
