7eb5376616eb070b4c837c6636ce9cf04def1de2
MyManVan <kcjackkelly@gmail.com>
2020-05-10 17:03:25 -0500

    Added route for static build resources

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,28 @@
 import { Router, Request, Response } from "express";
 import fs from "fs";
+import path from "path";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.type("html");
   fs.readFile("../../client/build/index.html", (err, data: Buffer) => {
-    console.log(err);
     res.send(data);
   });
+});
+
+router.get("/static/:folder/:filename", (req: Request, res: Response) => {
+  const { folder, filename } = req.params;
+
+  fs.readFile(
+    `../../client/build/static/${folder}/${filename}`,
+    (err, data: Buffer) => {
+      if (folder === "media") {
+      } else {
+        res.type(folder);
+      }
+      res.send(data);
+    }
+  );
 });
 
 export default router;
