096490661473b173dd4f41e1986542bb3c648422
MyManVan <kcjackkelly@gmail.com>
2020-05-12 11:55:47 -0500

    Added node mailer for form smtp

--- server/src/routes/form.ts
+++ server/src/routes/form.ts
@@ -1,15 +1,29 @@
 import { Router, Request, Response } from "express";
-import { sanitizeBody } from "express-validator/filter";
-import { body, validationResult } from "express-validator/check";
+import mailer from "nodemailer";
 
 const router = Router();
 
-router.post("/", (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
   const { name, email, message } = req.body;
 
-  res.send("Hello Inventory");
+  let testAccount = await mailer.createTestAccount();
 
-  // console.log(req);
+  let transporter = mailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Jacks Peppers" <foo@example.com>',
+    to: "test@gmail.com",
+    subject: "Jacks Peppers Form",
+    text: `Name: ${name}, Email: ${email}, Message: ${message}`,
+    html: `Name: ${name}<br>Email: ${email}<br>Message: ${message}`,
+  });
+
   console.log(req.body);
 });
 
