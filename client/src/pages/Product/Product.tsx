83caffacf5c761e93ce14162b042c6766c3a89d8
MyManVan <kcjackkelly@gmail.com>
2020-05-15 14:17:16 -0500

    Added handling for invalid product id

--- client/src/pages/Product/Product.tsx
+++ client/src/pages/Product/Product.tsx
@@ -5,7 +5,7 @@ import React, {
   SetStateAction,
   useEffect,
 } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { ColorKind } from "../../common/color";
 import { heatSwitch, heatSwitchColor } from "../../common/heat";
 import "./index.scss";
@@ -30,7 +30,7 @@ interface ProductItem {
   price: number;
   quantity: number;
   seedCount: number;
-  _id: string;
+  _id: string | boolean;
 }
 
 const Product: FC<Props> = (props) => {
@@ -46,24 +46,24 @@ const Product: FC<Props> = (props) => {
     price: 0,
     quantity: 1,
     seedCount: 0,
-    _id: "",
+    _id: false,
   });
 
   useEffect((): (() => void | undefined) => {
-    document.title = `${product.name} | Jack's Peppers`;
+    document.title = `Loading... | Jack's Peppers`;
     let isSubscribed = true;
-
-    if (isSubscribed && product.image) {
-      import(`../../assets/images/${product.image}`).then((image) =>
-        setImagePath(image.default)
-      );
-    }
 
     fetch(`/api/inventory/${id}`)
       .then((result) => {
         result.json().then((json) => {
           console.log(json);
           setProduct(json);
+          if (isSubscribed && json.image) {
+            import(`../../assets/images/${json.image}`).then((image) =>
+              setImagePath(image.default)
+            );
+          }
+          document.title = `${json.name} Pepper | Jack's Peppers`;
         });
       })
       .catch((err) => {
@@ -73,89 +73,99 @@ const Product: FC<Props> = (props) => {
     return () => {
       isSubscribed = false;
     };
-  }, [product.image, product.name]);
+  }, []);
 
   return (
     <main className="product">
-      <div className="product-info">
-        <div className="product-info--preview">
-          <div className="product-info--preview-main">
-            <img
-              src={imagePath}
-              alt={`${product.name} product.`}
-              title={`${product.name} product.`}
-            />
-          </div>
-          <ul className="product-info--preview-list">
-            <li className="product-info--preview-list--item">
-              <img
-                src={imagePath}
-                alt={`${product.name} product.`}
-                title={`${product.name} product.`}
-              />
-            </li>
-            <li className="product-info--preview-list--item">
-              <img
-                src={imagePath}
-                alt={`${product.name} product.`}
-                title={`${product.name} product.`}
-              />
-            </li>
-            <li className="product-info--preview-list--item">
-              <img
-                src={imagePath}
-                alt={`${product.name} product.`}
-                title={`${product.name} product.`}
-              />
-            </li>
-            <li className="product-info--preview-list--item">
-              <img
-                src={imagePath}
-                alt={`${product.name} product.`}
-                title={`${product.name} product.`}
-              />
-            </li>
-          </ul>
-        </div>
-        <div className="product-info--main">
-          <h1 className="product-info--main-title">{product.name}</h1>
-          <h3 className="product-info--main-price">
-            ${product.price.toFixed(2)} - {product.seedCount}+ Seeds -{" "}
-            <span style={{ color: heatSwitchColor(product.heat) }}>
-              {heatSwitch(product.heat)} Pepper
-            </span>
-          </h3>
-          <p className="product-info--main-description">
-            {product.description}
+      {!product._id ? (
+        <div className="product-error">
+          <h1 className="product-error--title">Product not found</h1>
+          <p className="product-error--description">
+            The product you are searching for does not exist.
           </p>
-          <button
-            className="product-info--main-add"
-            onClick={() => {
-              addItem(setCart, id);
-            }}
-            style={{
-              backgroundColor:
-                product.quantity === 0
-                  ? "gray"
-                  : checkDuplicate(cart, id)
-                  ? "gray"
-                  : "auto",
-              pointerEvents:
-                product.quantity === 0
-                  ? "none"
-                  : checkDuplicate(cart, id)
-                  ? "none"
-                  : "auto",
-            }}
-          >
-            {product.quantity === 0
-              ? "Out of stock"
-              : checkDuplicate(cart, id)
-              ? "In cart"
-              : "Add to cart"}
-          </button>
+          <Link to="/shop">Shop</Link>
         </div>
-      </div>
+      ) : (
+        <div className="product-info">
+          <div className="product-info--preview">
+            <div className="product-info--preview-main">
+              <img
+                src={imagePath}
+                alt={`${product.name} product.`}
+                title={`${product.name} product.`}
+              />
+            </div>
+            <ul className="product-info--preview-list">
+              <li className="product-info--preview-list--item">
+                <img
+                  src={imagePath}
+                  alt={`${product.name} product.`}
+                  title={`${product.name} product.`}
+                />
+              </li>
+              <li className="product-info--preview-list--item">
+                <img
+                  src={imagePath}
+                  alt={`${product.name} product.`}
+                  title={`${product.name} product.`}
+                />
+              </li>
+              <li className="product-info--preview-list--item">
+                <img
+                  src={imagePath}
+                  alt={`${product.name} product.`}
+                  title={`${product.name} product.`}
+                />
+              </li>
+              <li className="product-info--preview-list--item">
+                <img
+                  src={imagePath}
+                  alt={`${product.name} product.`}
+                  title={`${product.name} product.`}
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="product-info--main">
+            <h1 className="product-info--main-title">{product.name}</h1>
+            <h3 className="product-info--main-price">
+              ${product.price.toFixed(2)} - {product.seedCount}+ Seeds -{" "}
+              <span style={{ color: heatSwitchColor(product.heat) }}>
+                {heatSwitch(product.heat)} Pepper
+              </span>
+            </h3>
+            <p className="product-info--main-description">
+              {product.description}
+            </p>
+            <button
+              className="product-info--main-add"
+              onClick={() => {
+                addItem(setCart, id);
+              }}
+              style={{
+                backgroundColor:
+                  product.quantity === 0
+                    ? "gray"
+                    : checkDuplicate(cart, id)
+                    ? "gray"
+                    : "auto",
+                pointerEvents:
+                  product.quantity === 0
+                    ? "none"
+                    : checkDuplicate(cart, id)
+                    ? "none"
+                    : "auto",
+              }}
+            >
+              {product.quantity === 0
+                ? "Out of stock"
+                : checkDuplicate(cart, id)
+                ? "In cart"
+                : "Add to cart"}
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
