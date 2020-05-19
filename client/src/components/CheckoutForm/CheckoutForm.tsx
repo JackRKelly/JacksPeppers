5737ba59c19b181599228d385756d35531c5e21b
MyManVan <kcjackkelly@gmail.com>
2020-05-19 15:35:12 -0500

    Added on submit handler

--- client/src/components/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,18 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, FormEvent } from "react";
 import "./index.scss";
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  Elements,
-  CardElement,
-  useStripe,
-  useElements,
-} from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { StripeCardElement, Stripe } from "@stripe/stripe-js";
 
 const CheckoutForm: FC = () => {
   const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const {
+      error,
+      paymentMethod,
+    } = await (stripe as Stripe).createPaymentMethod({
+      type: "card",
+      card: elements?.getElement(CardElement) as StripeCardElement,
+    });
+
+    if (!error) {
+      console.log(paymentMethod);
+      const { id } = paymentMethod;
+    }
+  };
 
   return (
-    <form className="checkout-form">
+    <form onSubmit={handleSubmit} className="checkout-form">
       <h2>Checkout - 20$</h2>
       <CardElement />
       <button type="submit" disabled={!stripe}>
