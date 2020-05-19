6adc293b68f3c30ddc5cdd539daf1fbc6b7447a8
MyManVan <kcjackkelly@gmail.com>
2020-05-19 18:53:04 -0500

    Added payment intent try catch for payment api route

--- server/src/routes/payment.ts
+++ server/src/routes/payment.ts
@@ -1,44 +1,31 @@
 import { Router, Request, Response } from "express";
-import mongoose from "mongoose";
-import { Product } from "../models/product";
-import { v4 as uuidv4 } from "uuid";
-const stripe = require("stripe")(process.env.stripe_sec);
 
+const stripe = require("stripe")(process.env.STRIPE_SEC);
 const router = Router();
 
-router.post("/", (req: Request, res: Response) => {
-  const { cart, token } = req.body;
+router.post("/", async (req: Request, res: Response) => {
+  const { cart, id, amount } = req.body;
 
   console.log(cart);
-  console.log(token);
 
-  const idempotencyKey = uuidv4();
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Pepper seeds. Jacks Peppers",
+      payment_method: id,
+      confirm: true,
+    });
 
-  return stripe.customers
-    .create({
-      email: token.email,
-      source: token.id,
-    })
-    .then((customer: any) => {
-      stripe.charges.create(
-        {
-          amount: 0,
-          currency: "usd",
-          customer: customer.id,
-          receipt_email: token.email,
-          description: "product name",
-          shipping: {
-            name: token.card.name,
-            address: {
-              country: token.card.address_country,
-            },
-          },
-        },
-        { idempotencyKey }
-      );
-    })
-    .then((result: any) => res.status(200).json(result))
-    .catch((err: any) => console.error(err));
+    console.log(payment);
+
+    res.status(200).json({
+      confirm: "Success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 export default router;
