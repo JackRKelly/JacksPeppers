132b3bc8ffcb7c6967296502d8c1914fc31f1b53
MyManVan <kcjackkelly@gmail.com>
2020-05-19 20:39:01 -0500

    Added checkout modal elements

--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -31,45 +31,50 @@ const Cart: FC<Props> = (props) => {
   }, [cart, setIsLoading]);
 
   return (
-    <main className="cart">
-      <header className="hero-section">
-        <h1 className="hero-section--title">Your Cart</h1>
-      </header>
+    <>
+      <div className="checkout-modal">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm cart={cart} />
+        </Elements>
+      </div>
+      <main className="cart">
+        <header className="hero-section">
+          <h1 className="hero-section--title">Your Cart</h1>
+        </header>
 
-      {cart.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        <div className="cart-list">
-          <table className="cart-list--table">
-            <thead>
-              <tr>
-                <th scope="col">Product</th>
-                <th scope="col">Price</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Total</th>
-              </tr>
-            </thead>
-            <tbody className="cart-list--table-body">
-              {cart.map((cartItem, index) => (
-                <CartListItem
-                  id={cartItem.id}
-                  quantity={cartItem.quantity}
-                  setCart={setCart}
-                  index={index}
-                  cartLength={cart.length}
-                  setIsLoading={setIsLoading}
-                  key={typeof cartItem.id === "string" ? cartItem.id : index}
-                />
-              ))}
-            </tbody>
-          </table>
-          <CartSummary cart={cart} />
-          <Elements stripe={stripePromise}>
-            <CheckoutForm cart={cart} />
-          </Elements>
-        </div>
-      )}
-    </main>
+        {cart.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          <div className="cart-list">
+            <table className="cart-list--table">
+              <thead>
+                <tr>
+                  <th scope="col">Product</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Total</th>
+                </tr>
+              </thead>
+              <tbody className="cart-list--table-body">
+                {cart.map((cartItem, index) => (
+                  <CartListItem
+                    id={cartItem.id}
+                    quantity={cartItem.quantity}
+                    setCart={setCart}
+                    index={index}
+                    cartLength={cart.length}
+                    setIsLoading={setIsLoading}
+                    key={typeof cartItem.id === "string" ? cartItem.id : index}
+                  />
+                ))}
+              </tbody>
+            </table>
+            <CartSummary cart={cart} />
+            <button>Checkout</button>
+          </div>
+        )}
+      </main>
+    </>
   );
 };
 
